c832d590c1e1f4a26f5fcf60ee7a4890cb82c22b
RomanNTA <stivi@centrum.cz>
2025-02-17 23:07:55 +0100

    Delete React1/src directory

--- React1/src/functions/validateFloat.js
+++
@@ -1,6 +0,0 @@
-const validateFloat = (input) => {
-  const num = parseFloat(input);
-  return !isNaN(num) && input == num;
-}
-
-export default validateFloat;
--- React1/src/functions/validatePositiveInt.js
+++
@@ -1,6 +0,0 @@
-const validatePositiveInt = (input) => {
-  const num = parseInt(input);
-  return !isNaN(num) && input == num && num > 0;
-};
-
-export default validatePositiveInt;
