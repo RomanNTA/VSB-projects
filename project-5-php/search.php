f4e66a2eac35a78298712e1d60df6751fcd32cd5
RomanNTA <stivi@centrum.cz>
2025-03-07 14:27:57 +0100

    Add files via upload

--- project-5-php/search.php
+++ project-5-php/search.php
@@ -1,7 +1,4 @@
 <?php
-
-use Dom\XPath;
-
 header("Expires: Mon, 01 Jul 2024 05:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-cache, must-revalidate");  // HTTP/1.1
